a347690d75a611fd0ecb931613c99e1160dd0aaf
anthonygoslar <30745006+anthonygoslar@users.noreply.github.com>
2024-08-22 09:00:59 +0100

    renamed apps

---
+++ apps/profiles/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from . import models
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "pkid", "user", "gender", "phone_number", "country", "city"]
+    list_filter = ["gender", "country", "city"]
+    list_display_links = ["id", "pkid", "user"]
